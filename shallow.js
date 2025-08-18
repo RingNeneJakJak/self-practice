8ae25f0731d5dce9c02eba4eeca28df4e01a8acc
Nantapat Chantachoosak <RingNeneJakJak@users.noreply.github.com>
2025-08-19 01:09:39 +0700

    self practice

---
+++ shallow.js
@@ -0,0 +1,29 @@
+// Shallow Equality
+let book1 = {
+  isbn: 123456789,
+  title: "JavaScript",
+}
+
+let book2 = {
+  isbn: 123456789,
+  title: "JavaScript",
+}
+
+function shallowEquality(object1, object2){
+  const keys1 = Object.keys(object1)
+  const keys2 = Object.keys(object2)
+// เอาค่า array of keys
+  if(keys1.length !== keys2.length){
+    return false
+  }
+
+  for(let key of keys1){
+    if(object1[key] !== object2[key]){
+        //สามารถเข้าถึงค่า key ได้โดยใช้ []
+      return false
+    }
+  }
+  return true
+}
+
+console.log("shallow equality: " + shallowEquality(book1, book2)) // true
